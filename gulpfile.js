a255b9dce632108424aa1a04d623999c481901c7
juleasGoullee <julesgoullee@gmail.com>
2016-01-20 01:34:47 +0100

    get tweet stream

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')();
+const webpack = require('webpack');
+const merge = require('merge-stream');
+const production = process.argv.indexOf('--production') > -1;
+
+const paths = {
+  'dist': 'client/dist',
+  'index': 'client/index.html',
+  'webpack': {
+    'rootDir': ['./client/src/modules', './client/src/config', './node_modules']
+  },
+  'css': 'client/css/app.css'
+};
+
+gulp.task('static', () => {
+  
+  return merge(
+    gulp.src(paths.css)
+      .pipe(gulp.dest(`${paths.dist}/css`) ),
+
+    gulp.src(paths.index)
+      .pipe(gulp.dest(paths.dist) )
+  );
+  
+});
+gulp.task('webpack', (cb) => {
+  
+  webpack({
+    'entry': {
+      'index': [
+        './client/src/app.js'
+      ]
+    },
+    'output': {
+      'path': paths.dist,
+      'filename': 'scripts/bundle.js',
+      'publicPath': './client/src/modules'
+    },
+    'resolve': {
+      'modulesDirectories': paths.webpack.rootDir,
+      'extensions': ['', '.js', '.json']
+    },
+    'module': {
+      'loaders': [
+        { 'test': /src\/.*\.js$/, 'loader': 'babel?presets[]=es2015,cacheDirectory' }
+      ]
+    },
+    'plugins': [
+      new webpack.optimize.DedupePlugin()
+    ].concat(production ? [
+      new webpack.optimize.UglifyJsPlugin({
+        'compress': {
+          'warnings': false
+        }
+      })] : [])
+
+  }, (err) => {
+    
+    if(err){
+      
+      throw new $.util.PluginError('webpack', err);
+      
+    }
+    
+    cb();
+    
+  });
+  
+});
+
+gulp.task('dev-front-w', ['static', 'webpack'], () => {
+  
+  gulp.watch([
+    'client/src/**/*.js'
+  ], ['static', 'webpack']);
+
+});
+
+gulp.task('default', ['dev-front-w']);
---
+++ server/getTweets.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const Twitter = require('twitter');
+
+const client = new Twitter({
+  'consumer_key': '',
+  'consumer_secret': '',
+  'access_token_key': '',
+  'access_token_secret': ''
+});
+
+const parisBoundingBox = {
+  'sw': '2.23,48.82',
+  'ne': '2.44,48.90'
+};
+
+let nbTweetWithoutCoordinates = 0;
+
+module.exports = (cb) => {
+
+  client.stream('statuses/filter', { 'locations': `${parisBoundingBox.sw},${parisBoundingBox.ne}` }, (stream) => {
+
+    console.log(`Listen stream of tweet from location`);
+
+    stream.on('data', (tweet) => {
+      
+      if(tweet && tweet.geo && tweet.geo.coordinates){
+        console.log(tweet.text);
+        cb(tweet);
+        
+      } else{
+        
+        nbTweetWithoutCoordinates += 1;
+        console.info(`Tweet incomplete data nb ${nbTweetWithoutCoordinates}`);
+        
+      }
+
+    });
+
+    stream.on('error', (error) => {
+
+      throw error;
+
+    });
+
+  });
+
+};
